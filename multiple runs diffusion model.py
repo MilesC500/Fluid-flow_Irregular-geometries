ae828a11cb91028fec43d87f99fb1d29727ac6b2
MilesC500 <mccollins@cpp.edu>
2026-08-14 22:50:27 -0700

    Added a histogram of final positions!

--- multiple runs diffusion model.py
+++ multiple runs diffusion model.py
@@ -19,6 +19,25 @@ for i in range(1,n_trajectories):
     for i in range(1,n_trajectories):
         plt.plot(positions_2D[i,:])
         
+#struggling to get a histogram of the final positions of each trajectory12:30pm
+#trying to print a 1-D array of the final 2D positions from first to 50th trajectory 12:41pm
+path_ending_pos_iterable = (positions_2D[i,n_steps-1] for i in range(1,n_trajectories))
+dist1 = np.fromiter(path_ending_pos_iterable,int) 
+#print(dist1)
+
+#success!going to try to make a histogram using dist1 12:51pm
+
+fig, axs = plt.subplots(1,2, sharey=True, tight_layout=True)
+axs[0].hist(dist1, bins=n_bins)
+    
+
+      
+ 
+#success!going to try to make an animation going the distribution for 1000 steps 1:00pm
+
+
+  
+
 plt.xlabel('steps')
 plt.ylabel('positions')
 plt.show()
